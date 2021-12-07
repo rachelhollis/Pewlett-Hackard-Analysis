62ac18e4e00e3d15088e52edbd1e526d490f0b10
rachelhollis <rachel.hollis@live.com>
2021-12-07 17:01:14 -0600

    adding query

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -43,3 +43,102 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- create a new table with retirement elegibile employees with emp_no
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+SELECT * FROM retirement_info;
+
+
+-- joining departments and dept_manager tables
+SELECT d.dept_name, 
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT COUNT(first_name) FROM current_emp;
+-- 33118
+
+-- employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--Employee Information: A list of employees containing their 
+-- unique employee number, their last name, first name, gender, and salary
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+select * from emp_info;
+
+-- Management: A list of managers for each department, 
+-- including the department number, name, and the manager's employee number, 
+-- last name, first name, and the starting and ending employment dates
+-- 
+
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+--INTO manager_info
+FROM dept_manager as dm
+	INNER JOIN departments as d
+		on (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce
+		on (dm.emp_no = ce.emp_no);
+
+-- Department Retirees: An updated current_emp list that includes everything 
+-- it currently has, but also the employee's departments
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+--INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no);
